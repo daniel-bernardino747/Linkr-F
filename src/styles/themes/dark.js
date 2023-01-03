244cb79047e45136479456bf8d74de9af0114974
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-01-02 22:06:51 -0300

    feat: add dark theme setup and reset styles

---
+++ src/styles/themes/dark.js
@@ -0,0 +1,31 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: 'rgba(69, 153, 21, 1)',
+    secundary: 'rgba(72, 139, 34, 1)',
+
+    highlight: 'rgba(34, 34, 34, 1)',
+    background: 'rgba(22, 22, 22, 1)',
+
+    error: 'rgba(243, 80, 52, 1)',
+
+    text: {
+      primary: 'rgba(250, 250, 250, 1)',
+      secundary: 'rgba(208, 208, 208, 1)',
+      placeholder: 'rgba(97, 97, 97, 1)',
+
+      highlight: 'rgba(69, 153, 21, 1)',
+      reverseBackground: 'rgba(251, 251, 251, 1)',
+
+      error: 'rgba(243, 80, 52, 1)',
+    },
+
+    shadow: {
+      primary: 'rgba(111, 187, 70, 0.25)',
+      highlight: 'rgba(111, 187, 70, 0.4)',
+
+      error: 'rgba(243, 80, 52, 1)',
+    },
+  },
+}
