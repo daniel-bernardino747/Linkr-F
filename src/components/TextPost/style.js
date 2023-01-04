c568dd21dac370605e664715fd22d27e80b60d4f
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-01-04 10:18:09 -0300

    feat: create TextPost Component, redirect page to /hashtag/:tag,

---
+++ src/components/TextPost/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Container = styled.p`
+  padding: 0.5em 0;
+  line-height: 1.8em;
+  span {
+    font-weight: 700;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`
