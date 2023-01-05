f58f995bac07038c68ee5124ea95f43030a3c211
Vitor Torii <vitortorii@gmail.com>
2023-01-05 12:45:16 -0300

    feat: mapSnippet/feat: changePublishPost

---
+++ src/components/Header/style.js
@@ -0,0 +1,8 @@
+/* import styled from 'styled-components'
+
+export const ContHeader = styled.div`
+  width: 100vw;
+  height: 72px;
+  background-color: aqua;
+`
+ */
---
+++ src/components/Snippet/style.js
@@ -0,0 +1,74 @@
+import styled from 'styled-components'
+
+export const ContTimeline = styled.div`
+  display: flex;
+  width: 611px;
+  height: 276px;
+  background-color: #000000;
+  border-radius: 20px;
+  margin-bottom: 25px;
+
+  h1 {
+    margin-top: 20px;
+    font-size: 19px;
+    color: #ffffff;
+  }
+`
+
+export const ContIcons = styled.div`
+  width: 80px;
+  height: 50px;
+  display: grid;
+  justify-content: center;
+  margin-top: 20px;
+
+  img {
+    width: 50px;
+    height: 50px;
+    border-radius: 30px;
+  }
+`
+
+export const Content = styled.div`
+  h1 {
+    margin-bottom: 5px;
+  }
+`
+
+export const Banner = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 503px;
+  height: 155px;
+  border: 1px solid #4d4d4d;
+  border-radius: 5px;
+  margin-top: 10px;
+
+  div {
+    margin: 8px auto;
+  }
+
+  h1 {
+    width: 320px;
+    font-size: 16px;
+    color: #cecece;
+    margin-bottom: 10px;
+  }
+
+  h2 {
+    width: 320px;
+    font-size: 11px;
+    color: #9b9595;
+    margin-bottom: 10px;
+  }
+
+  h3 {
+    font-size: 11px;
+    color: #cecece;
+  }
+
+  img {
+    height: 153px;
+    width: 153px;
+  }
+`
