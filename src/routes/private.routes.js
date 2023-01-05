355a077fd3357903f40d14f650cef6b08169d776
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-01-05 20:19:06 -0300

    feat: add path /hashtag/:hashtag in routes

--- src/routes/private.routes.js
+++ src/routes/private.routes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import PrivatePages from '../components/Private'
+import Hashtag from '../pages/Hashtag'
 import Home from '../pages/Home'
 
 export default function PrivateRoutes() {
@@ -10,6 +11,7 @@ export default function PrivateRoutes() {
       <Route path="/" element={<PrivatePages />}>
         <Route index element={<Home />} />
         <Route path="/user" element={<Home />} />
+        <Route path="/hashtag/:hashtag" element={<Hashtag />} />
       </Route>
     </Routes>
   )
