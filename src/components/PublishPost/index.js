68d9f3f2cd7acecdbc27c8a38da4906401107877
Vitor Torii <vitortorii@gmail.com>
2023-01-06 15:35:00 -0300

    feat: postPublishForm

--- src/components/PublishPost/index.js
+++ src/components/PublishPost/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { postPublish } from '../../services/api/post.services'
 
 import { Publish, Form } from './style'
 
@@ -7,56 +9,49 @@ export default function PublishPost() {
     image:
       'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
   }
-  const [disable, setDisable] = React.useState(false)
-  const [message, setMessage] = React.useState('Publish')
-  const [bodyData, setBodyData] = React.useState({ url: '', text: '' })
+  const { register, handleSubmit, formState } = useForm()
+  const { isSubmitting, errors } = formState
 
-  function submitForm(e) {
-    e.preventDefault()
-    setDisable(true)
-    setMessage('Publishing...')
-    console.log(bodyData)
-
-    /*
-    const promise = axios.post('http://localhost:5000/posts', bodyData)
-    promise.then(() => {
-      setBodyData({ url: '', text: '' })
-      setDisable(false)
-      window.location.reload()
-    })
-    promise.catch(() => {
-      alert('Houve um erro ao publicar seu link')
-      setDisable(false)
-    })
-    */
-  }
-
-  function sendInput(e) {
-    setBodyData({ ...bodyData, [e.target.name]: e.target.value })
+  function submitForm(data) {
+    return new Promise((resolve) =>
+      postPublish(data)
+        .then((sucess) => {
+          window.location.reload()
+          console.log(sucess.data)
+          resolve()
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    )
   }
 
   return (
     <Publish>
       <img src={data.image} alt="profileImg" />
-      <Form onSubmit={submitForm}>
+      <Form onSubmit={handleSubmit(submitForm)}>
         <p>What are you going to share today?</p>
         <input
           type="url"
           placeholder="http://..."
-          name="url"
-          onChange={sendInput}
-          disabled={disable}
-          required
+          {...register('url', {
+            required: true,
+          })}
         />
+        {errors?.url?.type === 'required' && <p>Url is required.</p>}
+        {errors?.url?.type === 'validate' && <p>Url is invalid.</p>}
+
         <input
           type="text"
           placeholder="Awesome article about..."
-          name="text"
-          onChange={sendInput}
-          disabled={disable}
+          {...register('text', { required: true, maxLength: 190 })}
         />
-        <button type="onSubmit" disabled={disable}>
-          {message}
+        {errors?.text?.type === 'required' && <p>Text is required.</p>}
+        {errors?.text?.type === 'maxLength' && (
+          <p>Text must have at max 190 characters.</p>
+        )}
+        <button disabled={isSubmitting}>
+          {isSubmitting ? 'Publishing...' : 'Publish'}
         </button>
       </Form>
     </Publish>
