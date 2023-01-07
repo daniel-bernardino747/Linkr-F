3787605759ef96dee3371d9dc18b6c8e66ecef47
JoaoGMagri <jgmagri@hotmail.com>
2023-01-07 10:10:10 -0300

    feat: header organization and implementation

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -4,9 +4,22 @@ export const ContHeader = styled.div`
   width: 100vw;
   height: 72px;
   background-color: #000000;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  box-sizing: border-box;
+  padding: 17px;
 
   h1 {
+    font-family: 'Passion One', cursive;
     font-size: 49px;
     font-weight: 700;
   }
+
+  img{
+    width: 53px;
+    height: 53px;
+    border-radius: 26.5px;
+  }
+
 `
