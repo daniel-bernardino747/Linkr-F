30fa0f8da11bf674a516fe2a653579131264dd25
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-01-09 17:43:20 -0300

    feat: protected layout pages on

--- src/pages/ProtectedLayout/index.js
+++ src/pages/ProtectedLayout/index.js
@@ -1,14 +1,10 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 
-import AuthContext from '../../contexts/auth.context'
-
 export default function ProtectedLayout() {
-  const { signed } = useContext(AuthContext)
-  const bla = 1
+  const token = localStorage.getItem('token')
 
-  if (1 !== bla) {
-    console.log(signed)
+  if (!token) {
     return <Navigate to="/oauth/login" />
   }
   return <Outlet />
