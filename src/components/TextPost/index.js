10143036fa443eaa8c699ee755edea39a1ed5256
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-01-10 08:21:03 -0300

    fix: redirect page when click in tag inline text

--- src/components/TextPost/index.js
+++ src/components/TextPost/index.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import { redirect } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Navigate } from 'react-router-dom'
 import { ReactTagify } from 'react-tagify'
 
 import * as S from './style'
 
 export default function TextPost({ text }) {
+  const [navigation, setNavigation] = useState(undefined)
   const viewHashtag = async (name) => {
     name = name.replace(/[.,;?!#@$%&*()]/gi, '')
-    redirect('/hashtag/' + name)
+    setNavigation(`/hashtag/${name}`)
   }
   return (
     <S.Container>
+      {navigation && <Navigate to={navigation} />}
+
       <ReactTagify colors="rgba(250, 250, 250, 1)" tagClicked={viewHashtag}>
         {text}
       </ReactTagify>
