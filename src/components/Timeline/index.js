69204327e68d2bde9b2ffaa1dcf2ab367bebf891
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-01-11 10:05:08 -0300

    fix: key component render was duplicate

--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -8,14 +8,14 @@ import { Title } from './style'
 export default function Timeline({ posts, loading }) {
   // const [loading, setLoading] = useState(true)
   // const { user } = useContext(AuthContext)
-  console.log(posts)
+  // console.log(posts)
 
   return (
     <>
       {posts.length === 0 && !loading && <Title>There are no posts yet</Title>}
       {loading && <LineWave color="white" />}
-      {posts?.map((post) => (
-        <Snippet key={post.id} {...post} />
+      {posts?.map((post, i) => (
+        <Snippet key={post.id ** i * i} {...post} />
       ))}
       <ModalComponent />
     </>
