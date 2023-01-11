e6b386ac1db398810a3062743a5ab9d53ced7384
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-01-10 22:27:25 -0300

    feat: create SearchBar component

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,20 +1,32 @@
 import React, { useState } from 'react'
 import { DebounceInput } from 'react-debounce-input'
+import { Link } from 'react-router-dom'
 
 import { useSearchContext } from '../../contexts/search.context'
 import * as S from './style'
 
-export default function SearchBar({ users }) {
+export default function SearchBar() {
   const [modal, setModal] = useState(false)
   const { searchResults, setSearchResults } = useSearchContext()
 
   const handleSearch = (e) => {
-    console.log(e.target.value.length)
     e.target.value.length > 0 ? setModal(true) : setModal(false)
 
     console.log(searchResults)
 
-    setSearchResults(e.target.value)
+    const resultsArray = searchResults.original.filter((user) =>
+      user.name.includes(e.target.value)
+    )
+
+    if (resultsArray.length === 0) {
+      console.log(resultsArray.length === 0)
+      return setSearchResults({
+        ...searchResults,
+        search: [{ name: 'Nenhum resultado encontrado.' }],
+      })
+    } else {
+      setSearchResults({ ...searchResults, search: resultsArray })
+    }
   }
   return (
     <div>
@@ -26,8 +38,13 @@ export default function SearchBar({ users }) {
           element={S.Input}
         />
         <S.ModalSearch open={modal}>
-          {users.map((i) => (
-            <S.ItemUser key={i.id}>{i.name}</S.ItemUser>
+          {searchResults.search.map((i) => (
+            <Link key={i.id} to={`/users/${i.id}`}>
+              <S.ItemUser key={i.id} notFound={!!searchResults.search[0].image}>
+                {i.image && <S.ImageUser src={i.image} alt={i.name} />}
+                <S.NameUser>{i.name}</S.NameUser>
+              </S.ItemUser>
+            </Link>
           ))}
         </S.ModalSearch>
       </S.Container>
--- src/components/SearchBar/style.js
+++ src/components/SearchBar/style.js
@@ -6,7 +6,7 @@ export const Container = styled.div`
   justify-content: space-between;
   align-items: center;
   width: 30em;
-  height: 2em;
+  height: 2.4em;
 `
 export const Input = styled.input`
   z-index: 2;
@@ -36,9 +36,32 @@ export const ModalSearch = styled.div`
   border-radius: 0 0 1em 1em;
   background-color: gray;
   gap: 0.5em;
+
+  a {
+    text-decoration: none;
+  }
 `
 
 export const ItemUser = styled.div`
+  display: flex;
+  align-items: center;
   width: 100%;
-  background-color: red;
+  padding: 0.0625em;
+  border-radius: 2em;
+  gap: 0.5em;
+  &:hover {
+    background-color: ${(props) => (props.notFound ? '#8e8e8e' : 'none')};
+    cursor: ${(props) => (props.notFound ? 'pointer' : 'initial')};
+  }
+`
+export const ImageUser = styled.img`
+  width: 10px;
+  height: 10px;
+  border: 0.0625em solid #333333;
+  border-radius: 1em;
+`
+export const NameUser = styled.p`
+  font-size: 1.2em;
+  color: black;
+  font-weight: 500;
 `
