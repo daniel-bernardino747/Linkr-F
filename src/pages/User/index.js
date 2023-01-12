286a26a4af20cea22b77ae7d72e9be512f64e11f
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-01-12 14:01:28 -0300

    feat: implement  useElementOnScreen in pages

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,11 +1,14 @@
-import React, { useEffect } from 'react'
-import { useLoaderData } from 'react-router'
+import React, { useEffect, useState } from 'react'
+import { useLoaderData, useParams } from 'react-router'
 
 import Header from '../../components/Header'
+import { LoadingPage } from '../../components/LoadingPage'
 import Snippet from '../../components/Snippet'
 import Main from '../../components/Template/Main'
 import Trending from '../../components/Trending'
 import { useSearchContext } from '../../contexts/search.context'
+import { useElementOnScreen } from '../../helpers/hooks/useElementOnScreen'
+import { api } from '../../services/api'
 import { user } from '../../services/api/post.services'
 import * as S from './style'
 
@@ -18,7 +21,7 @@ export const loader = async ({ params }) => {
     },
   }
   const { data } = await user(id, config)
-  console.log(data.users)
+  console.log(data.posts)
 
   return {
     ...data,
@@ -31,9 +34,39 @@ export const loader = async ({ params }) => {
 }
 
 export default function User() {
-  const { title, posts, hashtags, users, follow } = useLoaderData()
+  const { title, posts: apiPosts, hashtags, users, follow } = useLoaderData()
   const { searchResults, setSearchResults } = useSearchContext()
-  useEffect(() => setSearchResults({ ...searchResults, original: users }), [])
+  const { id: idUser } = useParams()
+
+  const [currentPage, setCurrentPage] = useState(1)
+  const [posts, setPosts] = useState(apiPosts)
+
+  const [sentinelRef, loading] = useElementOnScreen({
+    posts,
+    setCurrentPage,
+  })
+
+  useEffect(() => {
+    setSearchResults({ ...searchResults, original: users })
+    if (currentPage !== 1) {
+      ;(async () => {
+        const token = localStorage.getItem('token')
+        const config = {
+          headers: {
+            Authorization: 'Bearer ' + token,
+          },
+        }
+        const { data } = await api.get(
+          `/users/${idUser}?page=${currentPage}`,
+          config
+        )
+        const newPosts = data.posts
+        setPosts((currentPosts) => [...currentPosts, ...newPosts])
+        loading.set(false)
+      })()
+    }
+  }, [currentPage])
+
   return (
     <S.Container>
       <Header />
@@ -42,6 +75,7 @@ export default function User() {
           {posts?.map((post) => (
             <Snippet key={post.id} {...post} username={post.name} />
           ))}
+          <LoadingPage ref={sentinelRef} loading={loading} />
         </div>
         <Trending hashtagList={hashtags} />
       </Main>
